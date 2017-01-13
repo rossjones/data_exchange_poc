25a2dfd8948762ad89037dd857234bedcaa926a4
Ross Jones <ross@servercode.co.uk>
2017-01-13 13:48:01 +0000

    Do some redirect

--- web/controllers/consent_controller.ex
+++ web/controllers/consent_controller.ex
@@ -5,8 +5,19 @@ defmodule Exchange.ConsentController do
     render conn, "index.html"
   end
 
+  defp get_referer(conn) do
+    case List.keyfind(conn.req_headers, "referer", 0) do
+      {"referer", referer} ->
+        parts = URI.parse(referer)
+        "#{parts.scheme}://#{parts.authority}"
+      nil ->
+        "https://127.0.0.1:9000"
+    end
+  end
+
   def blue_badge(conn, %{"bearer"=> token}) do
-    render conn, "blue_badge.html", error: false
+    get_referer(conn) |> IO.inspect
+    render conn, "blue_badge.html", error: false, referer: get_referer(conn), token: "1010101010"
   end
 
   def blue_badge(conn, _params) do
